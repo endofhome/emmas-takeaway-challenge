1d9fbcd8f7481c29a0e2f981dc4ee84cf107dd3f
Tom Barnes <ruralmyth@gmail.com>
2018-09-19 21:27:25 +0100

    Pass in dishes to Menu
    In it's previous state, Menu had a hardcoded list of dishes. This is
    problematic as it means that testing any methods on Menu is difficult.
    As we keep finding out, in order to test things we really need to be
    able to pass their dependencies in. It also makes our code generally
    more flexible.
    
    The previous test was also a little bit vaccuous, since we're just testing an
    instance method on Menu. Personally I would argue that it's unnecessary,
    but I know people in the Ruby world like to do things a bit differently,
    and I defend their right to do so. In that spirit I've left in a test on
    the instance method. It's much nicer now though, since we can just pass
    in any old list of dishes and expect to get the same smaller one back.
    
    I've also gotten a bit bold and decided that I want to get back the list
    of dishes in a more human readable format. There was a printed_list
    method that was unfinished so I've just finished a working version of
    that. I think it could be nicer, but it works, so I'm just going to
    leave it as it is for now.

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,36 +2,18 @@ class Menu
 
   attr_reader :menu_list
 
-  def initialize
-    @menu_list = [ { "snack seaweed" => 1.95} ,
-              {"oven roasted garlic" => 1.95},
-              {"tamari & black sesame rice crackers" => 2.95},
-              {"wild rice with truffles" => 2.95},
-              {"natto with hot mustard and sesame oil" => 2.95},
-              {"kimchi" => 2.95},
-              {"pickled vegetables" => 2.95},
-              {"smokey garlic broccolini" => 3.95},
-              {"agedashi tofu with soy and ginger dipping sauce" => 4.95},
-              {"vegetable tempura with wasabi and miso sauce" => 5.95},
-              {"spicy vegan dumpling noodle soup" => 5.95},
-              {"smoky huitlacoche, chilli sauce, vegan cheese, avocado and quesadillas" => 5.95},
-              {"dark chocolate coated fresh strawberries" => 4.95},
-              {"cashew, pistachio and rose vegan cheesecake" => 4.95},
-              {"glass of ginger and tumeric kombucha" => 2.95},
-              {"glass of elderberry water kefir" => 2.95},
-              {"warm spiced chai" => 2.95 },
-              {"bottle of champagne" => 69.95}
-            ]
+  def initialize(dishes)
+    @menu_list = dishes
   end
 
   def printed_list
-    @menu_list.each do |item|
-      puts item.to_a
-      puts " "
+    formatted = []
+    @menu_list.each do |dish|
+      dish.each do |name, price|
+        formatted << "#{name}, £#{price}"
+      end
     end
+    formatted.join(", ")
   end
 
 end
-
-# menu = Menu.new
-# menu.printed_list
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,29 +2,16 @@ require 'menu'
 
 describe Menu do
 
-  let(:menu) { described_class.new }
-  #let(:list) { double :menu, :list => [{ :a => 6.95, :b => 8.95 }] }
+  let(:dishes) { [{ "Mud Pie" => 6.95}, {"Snozzcumbers" => 8.95 }] }
+  let(:menu) { described_class.new(dishes) }
 
   context "menu_list" do
-    it "provides a list of menu items and prices" do
-      expect(menu.menu_list).to eq([{ "snack seaweed" => 1.95} ,
-                {"oven roasted garlic" => 1.95},
-                {"tamari & black sesame rice crackers" => 2.95},
-                {"wild rice with truffles" => 2.95},
-                {"natto with hot mustard and sesame oil" => 2.95},
-                {"kimchi" => 2.95},
-                {"pickled vegetables" => 2.95},
-                {"smokey garlic broccolini" => 3.95},
-                {"agedashi tofu with soy and ginger dipping sauce" => 4.95},
-                {"vegetable tempura with wasabi and miso sauce" => 5.95},
-                {"spicy vegan dumpling noodle soup" => 5.95},
-                {"smoky huitlacoche, chilli sauce, vegan cheese, avocado and quesadillas" => 5.95},
-                {"dark chocolate coated fresh strawberries" => 4.95},
-                {"cashew, pistachio and rose vegan cheesecake" => 4.95},
-                {"glass of ginger and tumeric kombucha" => 2.95},
-                {"glass of elderberry water kefir" => 2.95},
-                {"warm spiced chai" => 2.95 },
-                {"bottle of champagne" => 69.95}])
+    it "has a list of dishes and prices" do
+      expect(menu.menu_list).to eq(dishes)
+    end
+
+    it "prints the menu in human readable format" do
+      expect(menu.printed_list).to eq("Mud Pie, £6.95, Snozzcumbers, £8.95")
     end
   end
 end
